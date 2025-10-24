df3a436fafb4caf2449fb6833c305c791828d09b
Alex <alexandrepinto586@gmail.com>
2025-10-24 16:54:22 -0300

    clean: Removendo códigos antigos

--- backend/server/schemas/feedback_schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Dict
-
-class FeedbackInput(BaseModel):
-    uid: str  # Mantém uid pois será usado para identificar o usuário existente
-    features: Dict[str, float]
-    user_feedback: int
