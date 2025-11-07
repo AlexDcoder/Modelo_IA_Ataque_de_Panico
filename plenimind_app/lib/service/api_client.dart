5367a4779dd10b3394faa1b79505953ff9831843
Alex <alexandrepinto586@gmail.com>
2025-11-07 08:15:53 -0300

    feat: Adicionar métodos de put e delete

--- plenimind_app/lib/service/api_client.dart
+++ plenimind_app/lib/service/api_client.dart
@@ -3,14 +3,19 @@ import 'package:http/http.dart' as http;
 
 class ApiClient {
   static final http.Client client = http.Client();
-  static const String baseUrl = "https://modelo-ia-ataque-de-panico.onrender.com";
+  static const String baseUrl =
+      "https://modelo-ia-ataque-de-panico.onrender.com";
 
-  Future<http.Response> post(String endpoint, Map<String, dynamic> body, {Map<String, String>? headers}) async {
+  Future<http.Response> post(
+    String endpoint,
+    Map<String, dynamic> body, {
+    Map<String, String>? headers,
+  }) async {
     final url = Uri.parse('$baseUrl/$endpoint');
 
     final defaultHeaders = {'Content-Type': 'application/json'};
     final mergedHeaders = {...defaultHeaders, if (headers != null) ...headers};
-    
+
     try {
       final response = await client.post(
         url,
@@ -23,7 +28,10 @@ class ApiClient {
     }
   }
 
-  Future<http.Response> get(String endpoint, {Map<String, String>? headers}) async {
+  Future<http.Response> get(
+    String endpoint, {
+    Map<String, String>? headers,
+  }) async {
     final url = Uri.parse('$baseUrl/$endpoint');
 
     final defaultHeaders = {'Accept': 'application/json'};
@@ -37,4 +45,42 @@ class ApiClient {
     }
   }
 
+  Future<http.Response> delete(
+    String endpoint, {
+    Map<String, String>? headers,
+  }) async {
+    final url = Uri.parse('$baseUrl/$endpoint');
+
+    final defaultHeaders = {'Accept': 'application/json'};
+    final mergedHeaders = {...defaultHeaders, if (headers != null) ...headers};
+
+    try {
+      final response = await client.delete(url, headers: mergedHeaders);
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<http.Response> put(
+    String endpoint,
+    Map<String, dynamic> body, {
+    Map<String, String>? headers,
+  }) async {
+    final url = Uri.parse('$baseUrl/$endpoint');
+
+    final defaultHeaders = {'Content-Type': 'application/json'};
+    final mergedHeaders = {...defaultHeaders, if (headers != null) ...headers};
+
+    try {
+      final response = await client.put(
+        url,
+        headers: mergedHeaders,
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
